18f903e91d197c813bd9e211368ae5e53035ce8f
Ryan McBride <Ryan-McBride@users.noreply.github.com>
2018-03-26 13:14:54 -0700

    Update setfiles.sh

--- setfiles.sh
+++ setfiles.sh
@@ -5,9 +5,11 @@ mkdir -p $DIRECTORY
 cp ~/.bash_profile $DIRECTORY'/bash_profile'
 cp ~/.aliases $DIRECTORY'/aliases'
 cp ~/.vimrc $DIRECTORY'/vimrc'
+cp ~/.bash_prompt $DIRECTORY'/bash_prompt'
 
 cp ./bash_profile ~/.bash_profile
 cp ./aliases ~/.aliases
 cp ./vimrc ~/.vimrc
+cp ./bash_prompt ~/.bash_prompt
 
 echo 'The systemfiles have been updated from the files in this directory'
