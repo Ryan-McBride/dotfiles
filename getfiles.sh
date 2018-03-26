e2af09a6d6db83cd257f0eb5fe80dfe4dff0dae2
Ryan McBride <Ryan-McBride@users.noreply.github.com>
2018-03-26 13:16:10 -0700

    Update getfiles.sh

--- getfiles.sh
+++ getfiles.sh
@@ -2,5 +2,6 @@
 cp ~/.bash_profile ./bash_profile
 cp ~/.aliases ./aliases
 cp ~/.vimrc ./vimrc
+cp ~/.bash_prompt ./bash_prompt
 
 echo 'The files in this directory have been replaced with the system files'
