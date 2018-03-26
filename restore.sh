048f121e2b2dd6e58b34b86d5f26479e07f54cf6
Ryan McBride <Ryan-McBride@users.noreply.github.com>
2018-03-26 13:15:32 -0700

    Update restore.sh
    added bash_prompt

--- restore.sh
+++ restore.sh
@@ -6,6 +6,7 @@ done
 cp .backup/$newest/bash_profile ./bash_profile 
 cp .backup/$newest/vimrc ./vimrc 
 cp .backup/$newest/aliases ./aliases 
+cp .bachup/$newest/bash_prompt ./bash_prompt
 
 echo 'The newest backup has been restored. Please run setfiles to impliment these dotfiles'
 
